6f8069de4c0aeb162bd864fb53631705f36ed69e
n4beel <nabeel3353045805@hotmail.com>
2025-04-23 15:00:55 +0500

    fixed wallet connection buttons

--- fe-poc/src/app/components/SolanaConnectWalletButton.tsx
+++ fe-poc/src/app/components/SolanaConnectWalletButton.tsx
@@ -2,11 +2,39 @@
 
 import React from 'react'
 import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
+import { useWallet } from '@solana/wallet-adapter-react';
+import { shortenAddress } from '../utils/utils';
 
 
 const SolanaConnectWalletButton = () => {
+    const solanaWallet = useWallet();
+
+
     return (
-        <WalletMultiButton style={{}} />
+        <WalletMultiButton style={{
+            backgroundColor: 'white',
+            color: 'black',
+            borderRadius: '40px',
+            fontFamily: `"Sora", sans-serif`,
+        }}>
+            {
+                solanaWallet.connected ? (
+                    <span style={{
+                        fontSize: '16px',
+                        fontWeight: 600,
+                    }}>
+                        {solanaWallet.publicKey ? shortenAddress(solanaWallet.publicKey.toBase58()) : ''}
+                    </span>
+                ) : (
+                    <span style={{
+                        fontSize: '16px',
+                        fontWeight: 600,
+                    }}>
+                        Connect Solana Wallet
+                    </span>
+                )
+            }
+        </WalletMultiButton>
 
     )
 }
--- fe-poc/src/app/utils/utils.ts
+++ fe-poc/src/app/utils/utils.ts
@@ -4,3 +4,7 @@ export const toBigIntTokenAmount = (amount: string, tokenDecimals: number) => {
     const precision = new BigNumber(10).pow(tokenDecimals);
     return BigInt(new BigNumber(amount).multipliedBy(precision).toFixed(0));
 };
+
+export const shortenAddress = (address: string): string => {
+    return `${address.slice(0, 4)}...${address.slice(-4)}`;
+}
